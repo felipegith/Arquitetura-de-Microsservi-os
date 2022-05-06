609dc15f10c68170999ebc83f4a5c6d05d797f5b
felipegith <felipefknowx@gmail.com>
2022-05-06 15:52:02 -0300

    Configuração do GEEKSHOPPING API e criação do seu banco de dados

---
+++ GEEKSHOPPING.API/Model/Context/MySqlContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GEEKSHOPPING.API.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext() { }
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
---
+++ GEEKSHOPPING.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GEEKSHOPPING.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
